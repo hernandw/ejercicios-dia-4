8140db85b422f392fc450fc959eb1ab427a9ba0f
Williams Hernández <hernandw@gmail.com>
2024-02-01 12:43:33 -0300

    Ejercicos dia 4

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+    $('#parrafo').click(function () {
+        $(this).css("color", "red");
+        
+})
+
+ $("#parrafo").dblclick(function () {
+     $(this).css("color", "blue")
+     .css("background-color", "yellow")
+     
+ });
+    
+    
+    
+
+
+})
